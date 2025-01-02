8ed3feaf15b738cde9a5a47bef774dcc2faa6421
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-02 10:43:46 -0600

    First commit

---
+++ src/main/java/com/devstudios/redsocial/app/eureka/RedsocialEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.devstudios.redsocial.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedsocialEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedsocialEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstudios/redsocial/app/eureka/redsocial_eureka/RedsocialEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.redsocial.app.eureka.redsocial_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedsocialEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
